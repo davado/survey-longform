e50da11b4ae99e9b5998f23f93d15cfe9e04831e
Dave Wong <dave@dhmw.net>
2015-11-19 12:06:01 +1100

    Update results page message.

---
+++ results.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head></head>
+<body>Your answers have been submitted. 
+	<br />thanks for taking part in the survey.</body>
+<pre>
+<?php
+	
+$response = $_POST;
+var_dump($response);
+
+?>
+</pre>
+</html>
